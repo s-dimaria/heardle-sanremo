79f6cd7203874b3bda5abe2a238e71bed1587af4
DavideBri <70480082+DavideBri@users.noreply.github.com>
2023-04-18 10:16:43 +0000

    trying new ad format

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,10 +6,10 @@ export default function Banner(): JSX.Element {
     const banner = useRef<HTMLDivElement>(null)
 
     const atOptions = {
-		key : 'd8eda912a7485ab7f3da1d35bd959721',
+		key : '55a8d1b09b6ef43598774f4aa5e64ef7',
 		format : 'iframe',
-		height : 90,
-		width : 728,
+		height : 50,
+		width : 320,
 		params : {}
 	}
     useEffect(() => {
@@ -17,7 +17,7 @@ export default function Banner(): JSX.Element {
         const conf = document.createElement('script')
         const script = document.createElement('script')
         script.type = 'text/javascript'
-        script.src = `//www.effectivecreativeformat.com/${atOptions.key}/invoke.js`
+        script.src = `://www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`
         conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
 
         if (banner.current) {
